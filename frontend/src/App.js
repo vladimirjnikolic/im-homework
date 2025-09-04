e78a1b76d0f43fce14160b99bb486b763a720cd0
Vlada <vladimir.j.nikolic@gmail.com>
2025-09-04 18:19:18 +0200

    Added main page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,12 @@
 import "./App.css";
 import { StyledEngineProvider } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import SelectImageSize from "components/SelectImageSize/SelectImageSize";
-import InputFileUpload from "components/InputFileUpload/InputFileUpload";
+import MainPage from "components/MainPage";
 
 function App() {
   return (
     <div className="App">
       <StyledEngineProvider injectFirst>
-        <Box sx={{ flexGrow: 1, p: 4 }}>
-          <Grid
-            container
-            spacing={2}
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid size={8}>
-              <SelectImageSize />
-            </Grid>
-            <Grid size={4}>
-              <InputFileUpload />
-            </Grid>
-          </Grid>
-        </Box>
+        <MainPage />
       </StyledEngineProvider>
     </div>
   );

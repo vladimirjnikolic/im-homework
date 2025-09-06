d90c53a76bab5003efd1666339bae800a7339000
Vlada <vladimir.j.nikolic@gmail.com>
2025-09-06 11:36:48 +0200

    Put error image to S3 when image processing failed

--- backend/worker/src/services/s3Service.ts
+++ backend/worker/src/services/s3Service.ts
@@ -5,8 +5,6 @@ import {
   S3Client,
 } from "@aws-sdk/client-s3";
 
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
 const s3Client = new S3Client({
   region: process.env.REGION,
   credentials: {
